47fadb0caffe426311eb854d7ce496dc7c521996
js <js@jsui-noteubug.local>
2023-05-11 01:40:59 +0900

    Add: make DM page

---
+++ components/ChatList/index.tsx
@@ -0,0 +1,12 @@
+import { ChatZone, Section } from '@components/ChatList/styles';
+import React from 'react';
+
+const ChatList = () => {
+  return (
+    <ChatZone>
+      <Section>구역</Section>
+    </ChatZone>
+  );
+};
+
+export default ChatList;
--- pages/DM/index.tsx
+++ pages/DM/index.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import { Container, Header } from '@pages/DM/styles';
+import gravata from 'gravatar';
+import fetcher from '@utils/fetcher';
+import useSWR from 'swr';
+import { useParams } from 'react-router';
+import ChatBox from '@components/ChatBox';
+import ChatList from '@components/ChatList';
 
 const DM = () => {
-  return <div>DM</div>;
+  const { workspace, id } = useParams<{ workspace: string; id: string }>();
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/users/${id}`, fetcher);
+  const { data: myData } = useSWR('/api/users', fetcher);
+  if (!userData || !myData) {
+    return null;
+  }
+  return (
+    <Container>
+      <Header>
+        <img src={gravata.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+      <ChatList />
+      <ChatBox chat="" />
+    </Container>
+  );
 };
 export default DM;
